f6059a381ae484e99378be38cf57a6ed498e8a9e
minwoo <minu0922@gmail.com>
2021-07-16 16:48:41 +0900

    4장

---
+++ src/main/java/com/withShoes/www/controller/posts/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.withShoes.www.controller.posts;
+
+import com.withShoes.www.service.posts.PostsService;
+import com.withShoes.www.dto.posts.PostsResponseDto;
+import com.withShoes.www.dto.posts.PostsSaveRequestDto;
+import com.withShoes.www.dto.posts.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findId(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+}
---
+++ src/main/java/com/withShoes/www/service/posts/PostsService.java
@@ -0,0 +1,36 @@
+package com.withShoes.www.service.posts;
+
+import com.withShoes.www.domain.posts.Posts;
+import com.withShoes.www.domain.posts.PostsRepository;
+import com.withShoes.www.dto.posts.PostsResponseDto;
+import com.withShoes.www.dto.posts.PostsSaveRequestDto;
+import com.withShoes.www.dto.posts.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
