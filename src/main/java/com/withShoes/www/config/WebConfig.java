d1fe45458c334a52ad39621ec7855b978b573613
minwoo <minu0922@gmail.com>
2021-07-24 15:19:36 +0900

    21.07.24 스프링부트 셋팅

---
+++ src/main/java/com/withShoes/www/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.withShoes.www.config;
+
+import com.withShoes.www.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
