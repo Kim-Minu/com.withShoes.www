33157749941959d91401d52e469f98ff5cd6dad2
minwoo <minu0922@gmail.com>
2021-07-24 15:19:14 +0900

    21.07.24 스프링부트 셋팅

--- src/main/java/com/withShoes/www/controller/IndexController.java
+++ src/main/java/com/withShoes/www/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.withShoes.www.controller;
 
+import com.withShoes.www.config.auth.LoginUser;
+import com.withShoes.www.config.auth.dto.SessionUser;
 import com.withShoes.www.dto.posts.PostsResponseDto;
 import com.withShoes.www.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
--- src/test/java/com/withShoes/www/controller/posts/PostsApiControllerTest.java
+++ src/test/java/com/withShoes/www/controller/posts/PostsApiControllerTest.java
@@ -1,31 +1,44 @@
 package com.withShoes.www.controller.posts;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.withShoes.www.domain.posts.Posts;
 import com.withShoes.www.domain.posts.PostsRepository;
 import com.withShoes.www.dto.posts.PostsSaveRequestDto;
 import com.withShoes.www.dto.posts.PostsUpdateRequestDto;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostsApiControllerTest {
     @LocalServerPort
     private int port;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -38,7 +51,16 @@ public class PostsApiControllerTest {
         postsRepository.deleteAll();
     }
 
+    @Before
+    public void setup(){
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_등록된다() throws Exception{
 
         String title = "title";
@@ -52,11 +74,18 @@ public class PostsApiControllerTest {
 
         String url = "http://localhost:" + port + "/api/v1/posts";
 
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+        //when
+        mvc.perform(post(url)
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(new ObjectMapper().writeValueAsString(requestDto))
+        ).andExpect(status().isOk());
 
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        //ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
 
+        //assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        //assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        //then
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
@@ -64,6 +93,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_수정된다() throws Exception{
         Posts savedPosts = postsRepository.save(Posts.builder()
                 .title("title")
@@ -81,13 +111,19 @@ public class PostsApiControllerTest {
                 .build();
 
         String url = "http://localhost:" + port + "/api/v1/posts/"+updateId;
-        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+      /*  HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
 
         ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);*/
+        //when
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto))
+        ).andExpect(status().isOk());
 
+        //then
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
